25c58c9bcb321fff31990eb6f172f9448cf5c5f3
Mischa Hildebrand <web@mischa-hildebrand.de>
2018-01-15 10:27:39 +0100

    Add documentation.

--- Blockchain/Blockchain.swift
+++ Blockchain/Blockchain.swift
@@ -10,8 +10,15 @@ import Foundation
 
 public struct Blockchain<T: DataConvertible> {
     
+    /// The chain of blocks.
     var blocks: [Block<T>] = []
     
+    /// Checks if a block in the chain is valid at the given index
+    /// by validating that the block's hash is correct and its previous hash
+    /// is equal to the previous block's hash.
+    ///
+    /// - Parameter index: The index of the block in the chain to be validated.
+    /// - Returns: `true` if the block is valid, else `false`.
     public func isBlockValid(at index: Int) -> Bool {
         let previousBlock = blocks[index - 1]
         let currentBlock = blocks[index]
