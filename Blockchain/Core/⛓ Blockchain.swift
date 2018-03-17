55ffabff26472ae89672e9b2b92525a5a1300d40
Mischa Hildebrand <web@mischa-hildebrand.de>
2018-03-17 12:40:17 +0100

    Use accessor method rather than modifying the `blocks` array directly.

--- Blockchain/Core/⛓ Blockchain.swift
+++ Blockchain/Core/⛓ Blockchain.swift
@@ -24,7 +24,7 @@ public struct Blockchain<T: DataConvertible> {
     }
     
     public init() {
-        blocks.append(genesisBlock)
+        add(genesisBlock)
     }
     
     // MARK: - Validation
